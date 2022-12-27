03a179dab6018d10de053ff85c84ce9430f536b0
Shubhasis sahoo <jitans008s@gmail.com>
2022-12-27 18:42:41 +0530

    complete i think

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import '../styles/App.css';
 const App = () => {
     return (
-        <>Now I can render any React component on any DOM node I want using ReactDOM.render</>
+        <>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </>
     )
 }
 
